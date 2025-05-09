b92d15da4d799803cc0ec2cf9cf2964e7258f168
SH-ES <shahina.hussains@gmail.com>
2025-05-09 22:06:22 +0500

    Add files via upload

---
+++ data_types_practice.py
@@ -0,0 +1,31 @@
+#Integer is whole number.
+#age=52
+#question_attempted=3
+#marks= 70
+#print("Age:", age)
+#print("Question_attempted:", question_attempted)
+#print("Obtained marks:'", marks)
+
+#String is a collection of data including text, characters and words. Numerals 0123 are used as text.
+#name="Shahina"
+#print(name)
+#message="I am learning Python."
+#print(message)
+#profession="Teaching and Training"
+#print("Profession:",profession)
+
+#Float is a number in decimal
+#weight=5.7
+#print("Weight:", weight)
+#height=6.1
+#print("Height:", height)
+#rainfall=50.9
+#print("Rainfall:", rainfall)
+
+#Boolean works according to condition and tells true and false. MY QUERY IS CAN ITS ANSWER BE YES AND NO TOO?
+#is_student=True
+#print("Is student:", is_student)
+#is_school=False
+#print("Is school:", is_school)
+is_active=True
+print("Is Active:", is_active)
